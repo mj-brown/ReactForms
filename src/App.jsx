7b881cbda583e44f92b3b399e38a67d4a66213bb
mj-brown <brown8mj5@gmail.com>
2023-12-20 16:30:36 -1000

    completed signup form.  authenticate and styling are left

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import { useState } from 'react'
 import './App.css'
+import SignUpForm from  './components/SignUpForm'
+import Authenticate from  './components/Authenticate'
 
 function App() {
   
 
   return (
     <>
-      <div>
-        
-      </div>      
+      <SignUpForm />
+      <Authenticate />      
     </>
   )
 }
